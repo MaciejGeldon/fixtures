4fb60b4694b707bfc48d9b8911ba660c69945c11
maciejg <einstein1905meister@gmail.com>
2021-05-12 22:38:26 +0200

    add bucket test and refactor

--- orders.py
+++ orders.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Order:
+class Item:
     name: str
     unit_price: float
     quantity: int = 0
@@ -10,3 +10,13 @@ class Order:
     @property
     def total_cost(self):
         return self.unit_price * self.quantity
+
+
+@dataclass
+class Bucket:
+    name: str
+    items: [Item]
+
+    @property
+    def total_cost(self):
+        return sum(item.total_cost for item in self.items)
--- test_orders.py
+++ test_orders.py
@@ -1,13 +1,21 @@
 import pytest
 
-from orders import Order
+from orders import Item, Bucket
 
 
 @pytest.fixture
-def order():
-    return Order('socks', 10, 3)
+def item():
+    return Item('socks', 10, 3)
+
+
+@pytest.fixture
+def bucket(item):
+    return Bucket('cloths', [item])
 
 
 class TestOrder:
-    def test_order_total_price(self, order):
-        assert order.total_cost == order.unit_price * order.quantity
+    def test_order_total_price(self, item):
+        assert item.total_cost == item.unit_price * item.quantity
+
+    def test_bucket_with_single_item(self, bucket, item):
+        assert item.total_cost == bucket.total_cost
